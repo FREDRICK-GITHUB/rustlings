e32f3d8c7ea10bdafd5c237392ac90a1a62301d9
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-01-21 07:21:29 +0300

    cleared enums exercises, last one was challenging a bit

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,7 +5,10 @@
 // I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -39,6 +42,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
+     
+        match message {
+            Message::ChangeColor(r, g, b) => self.change_color((255, 0, 255)),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(Point{x:10, y:15}),
+            Message::Quit => self.quit()
+        }
     }
 }
 
